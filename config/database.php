126079537a5af14a8a46ee710bdd1ab7ae81d1f9
ErickVgC <ebvegav@ucvvirtual.edu.pe>
2026-05-31 13:37:06 -0500

    Configurar variables de entorno para Render

--- config/database.php
+++ config/database.php
@@ -2,15 +2,11 @@
 
 function getDBConnection(): PDO
 {
-    $host = 'aws-1-us-east-2.pooler.supabase.com';
-    $port = '5432';
-    $name = 'postgres';
-
-    // IMPORTANTE
-    $user = 'postgres.lgiipitgsodpodbnryys';
-
-    // Tu contraseña de Supabase
-    $pass = 'Crud2026@Supabase#';
+    $host = getenv('DB_HOST');
+    $port = getenv('DB_PORT');
+    $name = getenv('DB_NAME');
+    $user = getenv('DB_USER');
+    $pass = getenv('DB_PASS');
 
     $dsn = "pgsql:host={$host};port={$port};dbname={$name};sslmode=require";
 
